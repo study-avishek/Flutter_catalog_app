cbf567ecff2ff34bc967db1b772ca55aa98eaf44
study-avishek <study-avishek@users.noreply.github.com>
2022-01-20 21:59:32 +0530

    day4

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -3,12 +3,60 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("this is login page"),
-      ),
-      body: Center(
-        child: Text("login page"),
+    return Material(
+      color: Colors.white,
+      child: Column(
+        children: [
+          SizedBox(
+            height: 40,
+          ),
+          Image.asset("assets/images/login.png"),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Login",
+                    style: TextStyle(
+                        fontSize: 22,
+                        color: Colors.blueGrey,
+                        fontWeight: FontWeight.w400),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter Username /Email Address",
+                    labelText: "Username",
+                  ),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter Password",
+                    labelText: "Password",
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    print("hi");
+                  },
+                  style: TextButton.styleFrom(),
+                  child: Text("Log in"),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
